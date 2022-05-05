8f2020482410e8254a70b87e063bc7db2b07a81c
EReeves27 <ethan27reeves@gmail.com>
2022-05-04 19:28:49 -0700

    Create Constructor.java

---
+++ Constructor.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class Constructor
+{
+    private ArrayList <String> words;
+    
+    public Constructor(ArrayList <String> words)
+    {
+        this.words = words;
+    }
+    
+    public ArrayList getWordArray()
+    {
+        return words;
+    }
+}
